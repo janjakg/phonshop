aed37012ff06263178717f6dc022fade9bdf5655
janjakg <janjakg@yahoo.fr>
2020-10-05 22:12:29 +0200

    création page DétailPhone

---
+++ src/components/DetailPhone.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>
+      Détails du téléphone:
+    </p>
+  </div>  
+</template>
+
+<script>
+export default {
+  name:'DetailPhone.vue'
+  
+}
+</script>
+
+<style scoped>
+
+</style>
