bf411de4660aab99a265050ea976ed2b33aaa2ae
janjakg <janjakg@yahoo.fr>
2020-10-12 15:25:44 +0200

    création page détailItem

---
+++ src/views/DetailItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="detail">
+    <h1>Détails du téléphone</h1>
+    <PhoneList v-for="item in phonelisting" :key="item.name"/>
+    <p>Prix:{{  }}</p>
+    <p>Couleur:{{ }}</p>
+    <p>Stockage:{{ }}Go</p>
+  </div>
+</template>
+
+<script>
+import PhoneList from '@/components/PhoneList.vue'
+  export default {
+    name: 'DetailItem',
+    components :{
+      PhoneList
+    },
+    data(){
+      return{
+        phonelisting:[{
+          name: "Galaxy",
+          price:2,
+          color:"blue",
+          capacity:150,
+        }]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
