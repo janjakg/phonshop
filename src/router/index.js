6949856c1285f9572b34b65d4dbfeb8ea870a4e6
janjakg <janjakg@yahoo.fr>
2020-10-12 15:26:06 +0200

    création route detailItem

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import DetailItem from '../views/DetailItem.vue'
 
 Vue.use(VueRouter)
 
@@ -17,11 +18,19 @@ const routes = [{
         // which is lazy-loaded when the route is visited.
         component: () =>
             import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/detail',
+        name: 'DetailItem',
+        component: DetailItem
     }
 ]
 
 const router = new VueRouter({
+    mode: 'history',
     routes
 })
+
+
 
 export default router
