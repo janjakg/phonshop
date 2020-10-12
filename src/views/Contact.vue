187f1b113028aa6d88e638b46632ac09bb68a010
janjakg <janjakg@yahoo.fr>
2020-10-12 16:47:58 +0200

    création d'une page contact

---
+++ src/views/Contact.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="contact">
+  <h1>Contactez nous:</h1>
+  <p>Adresse : {{ adress }} </p>
+  <p>Email : {{ email }} </p>
+  <p>Téléphone : {{ phone }} </p>
+</div>  
+</template>
+
+<script>
+export default {
+  name : 'Contact',
+  data(){
+    return{
+      adress:'1 rue de l\'unique source de vérité, 75001 Paris',
+      email:'phonshop@gmail.com',
+      phone: '01 02 03 44 55'
+    }
+  }
+}
+</script>
