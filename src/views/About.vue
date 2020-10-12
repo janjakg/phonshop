034f20b74d9a02f9dcafc79d347dd1bc0807ac47
janjakg <janjakg@yahoo.fr>
2020-10-12 16:48:20 +0200

    modification de la page about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Us</h1>
+    <p>Phoneshop began in 2004</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, in eaque, maxime iste exercitationem fugit
+      praesentium doloribus rerum accusamus aperiam, esse dolore nobis laudantium. Recusandae illum ullam expedita minus
+      quisquam!25</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut architecto ab, sint explicabo molestiae nihil
+      corrupti, amet maiores delectus quod tempore consequuntur quisquam, modi esse iusto quae magni nam nostrum atque
+      dolorem in rem beatae! Neque incidunt expedita doloribus. Ab tenetur ad fugiat explicabo, aspernatur veritatis
+      quos voluptate debitis minus?</p>
   </div>
 </template>
