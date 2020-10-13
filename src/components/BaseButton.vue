fca7a2c4d7266de93a2346f1cee93d53a32bf38a
janjakg <janjakg@yahoo.fr>
2020-10-13 18:35:51 +0200

    creation de BaseButton

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,12 @@
+<template>
+<!-- v-on nous permettra d'asigner les évènements à un élément du composant afin que Vue sache où écouter pour les évènements-->
+  <button v-on="$listeners">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'BaseButton'
+  }
+</script>
